f74f695d07b44267d100a62a9bed1e07476776a2
Vinit <vinit224488@gmail.com>
2023-10-06 06:26:36 +0530

    added comment for c.locals

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -7,6 +7,7 @@ import (
 )
 
 func GetSelf(c *fiber.Ctx) error {
+	// Retrieve the user data from the context. This data is typically set in previous middleware user authentication.
 	user := c.Locals("user").(models.UserResponse)
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "data": fiber.Map{"user": user}})
