d751e3c60b632c213a178501a007e3f5dc56d0da
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2023-10-17 05:47:51 +0530

    Configure CORS properly to allow only access from certain origins (#13)
    * add auth and login route
    
    * change db url for test
    
    * added cors for certain origin
    
    * move origin to env

--- routes/main.go
+++ routes/main.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"os"
+	"strings"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
@@ -13,13 +16,20 @@ func SetupV1Routes(db *bun.DB) *gin.Engine {
 	UserRoutes(v1, db)
 	AuthRoutes(v1, db)
 
+	allowedOrigins := strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = allowedOrigins
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept"}
+	config.AllowMethods = []string{"GET", "POST"}
+	config.AllowCredentials = true
+
+	router.Use(cors.New(config))
+
 	return router
 }
 
 func Listen(listenAddress string, db *bun.DB) {
 	router := SetupV1Routes(db)
-
-	// TODO: Configure CORS properly to allow only access from certain origins
-	router.Use(cors.Default())
 	router.Run(listenAddress)
 }
