790c208a00cbdc33e38f4569ee4046a893878f65
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-03 19:10:36 +0530

    route: added new route and middleware for user

--- routes/users.go
+++ routes/users.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 
+	"github.com/Real-Dev-Squad/tiny-site-backend/middlewares"
 	"github.com/Real-Dev-Squad/tiny-site-backend/models"
 	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
@@ -10,6 +11,7 @@ import (
 
 func UserRoutes(rg *gin.RouterGroup, db *bun.DB) {
 	users := rg.Group("/users")
+	users.Use(middleware.AuthMiddleware())
 
 	users.GET("", func(ctx *gin.Context) {
 
@@ -26,6 +28,44 @@ func UserRoutes(rg *gin.RouterGroup, db *bun.DB) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "users fetched successfully",
 			"data":    users,
+		})
+	})
+
+	users.GET("/:id",func(ctx *gin.Context) {
+		id := ctx.Param("id")
+
+		var user models.User
+		err := db.NewSelect().Model(&user).Where("id = ?", id).Scan(ctx)
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": "error",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "user fetched successfully",
+			"data":    user,
+		})
+	})
+
+	users.GET("/self", func(ctx *gin.Context) {
+		userEmail, _ := ctx.Get("user")
+
+		var user models.User
+		err := db.NewSelect().Model(&user).Where("email = ?", userEmail).Scan(ctx)
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": "error",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "user fetched successfully",
+			"data":    user,
 		})
 	})
 }
