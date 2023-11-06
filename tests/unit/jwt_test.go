1ceac5a9cfb859115b8009c6d736302b54fde92a
Vinit <vinit224488@gmail.com>
2023-11-06 19:05:56 +0530

    add test for token validation

--- tests/unit/jwt_test.go
+++ tests/unit/jwt_test.go
@@ -33,21 +33,51 @@ func TestGenerateJWT(t *testing.T) {
 }
 
 func TestVerifyJWT(t *testing.T) {
+	t.Run("ValidToken", func(t *testing.T) {
+		dummyUser := &models.User{
+			Email: "test@gmail.com",
+		}
+
+		validToken, generateTokenError := utils.GenerateToken(dummyUser)
+
+		if generateTokenError != nil {
+			t.Fatalf("Error: %v", generateTokenError)
+		}
+
+		email, validTokenError := utils.VerifyToken(validToken)
+
+		if validTokenError != nil {
+			t.Fatalf("Error: %v", validTokenError)
+		}
+
+		if email != dummyUser.Email {
+			t.Fatalf("Expected %v but got %v", dummyUser.Email, email)
+		}
+	})
+
+	t.Run("ExpiredToken", func(t *testing.T) {
+		expiredToken := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoiMjAyMy0xMC0wMVQxOTo1Njo0OS4zOTc5NzEyWiIsImlzcyI6Indpc2VlLWJhY2tlbmQifQ.h11JtaPg-ITKR8UXTyz_Q7pJU_3gYyXwIkqX7lI1UK2nVkvxQvkyN23-u3wj8fV5mNIvp-ePTOp-7odsPcGC_g"
+
+		_, expiredTokenError := utils.VerifyToken(expiredToken)
+
+		if expiredTokenError == nil {
+			t.Fatalf("Expected error but got nil")
+		}
+	})
+}
+
+func TestVerifyJWTForOneYear(t *testing.T) {
+	os.Setenv("JWT_VALIDITY_IN_HOURS", "8760")
+	defer os.Setenv("JWT_VALIDITY_IN_HOURS", "24")
+
 	dummyUser := &models.User{
 		Email: "test@gmail.com",
 	}
 
 	validToken, generateTokenError := utils.GenerateToken(dummyUser)
-	expiredToken := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoiMjAyMy0xMC0wMVQxOTo1Njo0OS4zOTc5NzEyWiIsImlzcyI6Indpc2VlLWJhY2tlbmQifQ.h11JtaPg-ITKR8UXTyz_Q7pJU_3gYyXwIkqX7lI1UK2nVkvxQvkyN23-u3wj8fV5mNIvp-ePTOp-7odsPcGC_g"
 
 	if generateTokenError != nil {
 		t.Fatalf("Error: %v", generateTokenError)
-	}
-
-	_, expiredTokenError := utils.VerifyToken(expiredToken)
-
-	if expiredTokenError == nil {
-		t.Fatalf("Expected error but got nil")
 	}
 
 	email, validTokenError := utils.VerifyToken(validToken)
