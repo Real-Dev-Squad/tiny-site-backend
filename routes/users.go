e3e25f43416da26441bd71679e2586ba487f9b66
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-06 06:50:56 +0530

    refactor: created separate controller for users

--- routes/users.go
+++ routes/users.go
@@ -1,71 +1,25 @@
 package routes
 
 import (
-	"net/http"
-
-	"github.com/Real-Dev-Squad/tiny-site-backend/middlewares"
-	"github.com/Real-Dev-Squad/tiny-site-backend/models"
-	"github.com/gin-gonic/gin"
-	"github.com/uptrace/bun"
+    "github.com/gin-gonic/gin"
+    "github.com/uptrace/bun"
+    "github.com/Real-Dev-Squad/tiny-site-backend/controllers"
+    "github.com/Real-Dev-Squad/tiny-site-backend/middlewares"
 )
 
 func UserRoutes(rg *gin.RouterGroup, db *bun.DB) {
-	users := rg.Group("/users")
-	users.Use(middleware.AuthMiddleware())
+    users := rg.Group("/users")
+    users.Use(middleware.AuthMiddleware())
 
-	users.GET("", func(ctx *gin.Context) {
+    users.GET("", func(ctx *gin.Context) {
+        controllers.GetUserList(ctx, db)
+    })
 
-		var users []models.User
-		err := db.NewSelect().Model(&users).OrderExpr("id ASC").Limit(10).Scan(ctx)
+    users.GET("/:id", func(ctx *gin.Context) {
+        controllers.GetUserByID(ctx, db)
+    })
 
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": "error",
-			})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "users fetched successfully",
-			"data":    users,
-		})
-	})
-
-	users.GET("/:id",func(ctx *gin.Context) {
-		id := ctx.Param("id")
-
-		var user models.User
-		err := db.NewSelect().Model(&user).Where("id = ?", id).Scan(ctx)
-
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": "error",
-			})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "user fetched successfully",
-			"data":    user,
-		})
-	})
-
-	users.GET("/self", func(ctx *gin.Context) {
-		userEmail, _ := ctx.Get("user")
-
-		var user models.User
-		err := db.NewSelect().Model(&user).Where("email = ?", userEmail).Scan(ctx)
-
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": "error",
-			})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "user fetched successfully",
-			"data":    user,
-		})
-	})
+    users.GET("/self", func(ctx *gin.Context) {
+        controllers.GetSelfUser(ctx, db)
+    })
 }
