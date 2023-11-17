1e38cbe66d4edf58e584f9c1064bef97b303d43b
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2023-11-17 18:34:51 +0530

    fix failing test (#40)

--- tests/integration/users_test.go
+++ tests/integration/users_test.go
@@ -6,8 +6,10 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Real-Dev-Squad/tiny-site-backend/models"
 	"github.com/Real-Dev-Squad/tiny-site-backend/routes"
 	"github.com/Real-Dev-Squad/tiny-site-backend/utils"
+	"github.com/gin-gonic/gin"
 	"github.com/uptrace/bun"
 )
 
@@ -25,17 +27,36 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+func generateValidAuthToken() string {
+	user := &models.User{
+		Username: "testuser",
+		Email:    "test@example.com",
+	}
+
+	token, err := utils.GenerateToken(user)
+	if err != nil {
+		panic(err)
+	}
+
+	return token
+}
+
 func TestGetUsers(t *testing.T) {
-	router := routes.SetupV1Routes(db)
+	router := gin.Default()
+	routes.UserRoutes(router.Group("/v1"), db)
 
 	w := httptest.NewRecorder()
+
+	token := generateValidAuthToken()
+
 	req, err := http.NewRequest("GET", "/v1/users", nil)
-
-	router.ServeHTTP(w, req)
-
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	req.AddCookie(&http.Cookie{Name: "token", Value: token})
+
+	router.ServeHTTP(w, req)
 
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d but got %d", http.StatusOK, w.Code)
