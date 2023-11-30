494ea80c63c9668c63bc533286883d0fda34f2a6
vinit <vinit224488@gmail.com>
2023-11-30 07:21:42 +0530

    test: add test for user response

--- tests/integration/users_test.go
+++ tests/integration/users_test.go
@@ -1,11 +1,15 @@
 package tests
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 
+	"time"
+
+	"github.com/Real-Dev-Squad/tiny-site-backend/dtos"
 	"github.com/Real-Dev-Squad/tiny-site-backend/models"
 	"github.com/Real-Dev-Squad/tiny-site-backend/routes"
 	"github.com/Real-Dev-Squad/tiny-site-backend/utils"
@@ -103,8 +107,6 @@ func TestGetUserById(t *testing.T) {
 	router := gin.Default()
 	routes.UserRoutes(router.Group("/v1"), db)
 
-	w := httptest.NewRecorder()
-
 	token := generateValidAuthToken()
 
 	req, err := http.NewRequest("GET", "/v1/users/1", nil)
@@ -114,10 +116,25 @@ func TestGetUserById(t *testing.T) {
 
 	req.AddCookie(&http.Cookie{Name: "token", Value: token})
 
+	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d but got %d", http.StatusOK, w.Code)
+	}
+
+	var response dtos.UserResponse
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Message != "user fetched successfully" {
+		t.Errorf("Expected message to be 'user fetched successfully' but got '%s'", response.Message)
+	}
+
+	if response.Data.ID != 1 {
+		t.Errorf("Expected user ID to be 1 but got %d", response.Data.ID)
 	}
 }
 
@@ -154,5 +171,25 @@ func TestGetUrlsByUserIdUnauthorized(t *testing.T) {
 
 	if w.Code != http.StatusUnauthorized {
 		t.Errorf("Expected status code %d but got %d", http.StatusUnauthorized, w.Code)
+	}
+}
+
+func TestURLCreationResponse(t *testing.T) {
+	now := time.Now()
+	response := dtos.URLCreationResponse{
+		Message:   "URL created successfully",
+		ShortURL:  "http://short.url",
+		CreatedAt: now,
+	}
+
+	if response.Message != "URL created successfully" {
+		t.Errorf("Expected message to be 'URL created successfully' but got '%s'", response.Message)
+	}
+
+	if response.ShortURL != "http://short.url" {
+		t.Errorf("Expected short URL to be 'http://short.url' but got '%s'", response.ShortURL)
+	}
+	if !response.CreatedAt.Equal(now) {
+		t.Errorf("Expected CreatedAt to be '%v' but got '%v'", now, response.CreatedAt)
 	}
 }
