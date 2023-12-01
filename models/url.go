bab22ad6064a46abbeea1bcf66fc3ad7043d61d9
vinit <vinit224488@gmail.com>
2023-12-01 22:53:47 +0530

    feat: add count and last access key in model

--- models/url.go
+++ models/url.go
@@ -9,12 +9,14 @@ import (
 type Tinyurl struct {
 	bun.BaseModel `bun:"table:tiny_url"`
 
-	ID          int64     `bun:"id,pk,autoincrement" json:"id"`
-	OriginalUrl string    `bun:"original_url,notnull" json:"originalUrl"`
-	ShortUrl    string    `bun:"short_url,unique,notnull" json:"shortUrl"`
-	Comment     string    `bun:"comment" json:"comment"`
-	UserID      int64     `bun:"user_id,rel:belongs-to,join:id" json:"userId"`
-	ExpiredAt   time.Time `bun:"expired_at,notnull" json:"expiredAt"`
-	CreatedAt   time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"createdAt"`
-	CreatedBy   string    `bun:"created_by,notnull" json:"createdBy"`
+	ID             int64     `bun:"id,pk,autoincrement" json:"id"`
+	OriginalUrl    string    `bun:"original_url,notnull" json:"originalUrl"`
+	ShortUrl       string    `bun:"short_url,unique,notnull" json:"shortUrl"`
+	Comment        string    `bun:"comment" json:"comment"`
+	UserID         int64     `bun:"user_id,rel:belongs-to,join:id" json:"userId"`
+	ExpiredAt      time.Time `bun:"expired_at,notnull" json:"expiredAt"`
+	CreatedAt      time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"createdAt"`
+	CreatedBy      string    `bun:"created_by,notnull" json:"createdBy"`
+	AccessCount    int64     `bun:"access_count,default:0" json:"accessCount"`
+	LastAccessedAt time.Time `bun:"last_accessed_at,nullzero" json:"lastAccessedAt"`
 }
