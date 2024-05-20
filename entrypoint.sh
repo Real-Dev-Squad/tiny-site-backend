4fb26e56c05ec88ce9be3497b03801b42237da8f
Prakash <om.pcprakash@gmail.com>
2024-05-20 16:04:26 +0530

    chore: add entrypoint.sh file to start server

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Function to check if the database is ready
+check_database() {
+    until nc -z -v -w30 db 5432
+    do
+        echo "Waiting for database connection..."
+        sleep 1
+    done
+}
+
+# Main entry point
+main() {
+    # Wait for the database to be ready
+    echo "Waiting for the database to be ready..."
+    check_database
+
+     # Run the database initialization.
+    echo "Initializing the database..."
+    /bin/bun/bun db init
+    
+    # Run the database migration.
+    echo "Migrating the database..."
+    /bin/bun/bun db init
+
+    # Run the main application
+    echo "Starting the application..."
+    exec /bin/server
+}
+
+# Call the main function
+main "$@"
