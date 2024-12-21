ca65323f1f598642ac5eabc4bd70750e3e0d756b
FMK2312 <fmkhandwala39@gmail.com>
2024-12-21 09:47:05 +0530

    config fix

--- controllers/url.go
+++ controllers/url.go
@@ -107,7 +107,13 @@ func CreateTinyURL(ctx *gin.Context, db *bun.DB) {
 		return
 	}
 
-	if strings.Contains(strings.ToLower(parsedUrl.Host),strings.ToLower(config.Domain)) {
+	parseConfig, errr := url.Parse(config.Domain);
+
+	if errr != nil {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(parsedUrl.Host),strings.ToLower(parseConfig.Host)) {
 			
 		path :=parsedUrl.Path
 	
